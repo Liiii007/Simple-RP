3a9b34544512d68b546c2aac73b1790bf4b826b1
Liiii <liiii007@outlook.com>
2023-09-27 22:45:27 +0800

    Build a new render pipeline -> Part1

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Simple-RP
-A custom SRP for unity
+
+A custom SRP for unity, following https://catlikecoding.com/unity/tutorials/custom-srp/
+THX!
+
+## Description
+
+A minimal render pipeline with SRP, not lighting, with post-processing/FXAA/TAA/particle/render-scale support
+
+## Goals
+
+- [x] Build a new Render Pipeline (Part1)
+- [ ] Write simple HLSL shader && Support SRP batcher (Part2)
+- [ ] Support Post-Processing stack && Bloom
+- [ ] Support HDR
+- [ ] Support Color Grading
+- [ ] Support Render Scale
+- [ ] Support FXAA
---
+++ Runtime/CameraRenderer.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public partial class CameraRenderer
+{
+    private const string BufferName = "Render Camera";
+
+    private Camera _camera;
+    private ScriptableRenderContext _context;
+    private readonly CommandBuffer _buffer = new CommandBuffer { name = BufferName };
+    private CullingResults _cullingResults;
+
+    public void Render(ScriptableRenderContext context, Camera camera)
+    {
+        _context = context;
+        _camera = camera;
+
+        PrepareBuffer();
+        PrepareForSceneWindow();
+        if (!Cull())
+        {
+            return;
+        }
+
+        Setup();
+        DrawVisibleGeometry();
+        DrawUnsupportedShaders();
+        DrawGizmos();
+        Submit();
+    }
+
+    private void Setup()
+    {
+        _context.SetupCameraProperties(_camera);
+        CameraClearFlags flags = _camera.clearFlags;
+        _buffer.ClearRenderTarget(flags <= CameraClearFlags.Depth, flags <= CameraClearFlags.Color,
+            flags == CameraClearFlags.Color ? _camera.backgroundColor.linear : Color.clear);
+        _buffer.BeginSample(SampleName);
+        ExecuteBuffer();
+    }
+
+    private void DrawVisibleGeometry()
+    {
+        var sortingSettings = new SortingSettings(_camera)
+        {
+            criteria = SortingCriteria.CommonOpaque
+        };
+        var drawingSettings = new DrawingSettings(UnlitShaderTagId, sortingSettings);
+        var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+
+        _context.DrawRenderers(
+            _cullingResults, ref drawingSettings, ref filteringSettings
+        );
+        _context.DrawSkybox(_camera);
+
+        sortingSettings.criteria = SortingCriteria.CommonTransparent;
+        drawingSettings.sortingSettings = sortingSettings;
+        filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+
+        _context.DrawRenderers(
+            _cullingResults, ref drawingSettings, ref filteringSettings
+        );
+    }
+
+    private void Submit()
+    {
+        _buffer.EndSample(SampleName);
+        ExecuteBuffer();
+        _context.Submit();
+    }
+
+    private void ExecuteBuffer()
+    {
+        _context.ExecuteCommandBuffer(_buffer);
+        _buffer.Clear();
+    }
+
+    private bool Cull()
+    {
+        if (_camera.TryGetCullingParameters(out var p))
+        {
+            _cullingResults = _context.Cull(ref p);
+            return true;
+        }
+
+        return false;
+    }
+}
